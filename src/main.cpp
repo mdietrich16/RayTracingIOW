06873d1ca025f2700c112527a28d3a2b51e94053
Maximilian Dietrich <m.dietrich16@web.de>
2021-11-03 23:30:46 +0100

    Now got antialiasing and fixed a bug in vec3

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 
 vec3 color(const ray &r, hitable *world)
 {
@@ -25,6 +26,7 @@ int main()
     file.open("pic.ppm");
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     file << "P3\n"
          << nx << " " << ny << "\n255\n";
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
@@ -35,16 +37,23 @@ int main()
     list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5);
     list[1] = new sphere(vec3(0.0, -100.5, -1.0), 100);
     hitable *world = new hitable_list(list, 2);
+    camera cam;
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 col(0.0, 0.0, 0.0);
             
-            vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+            for (int s = 0; s < ns; s++)
+            {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                
+                col += color(r, world);
+            }
+            
+            col /= float(ns);
             int ir = int(255.99 * col.r());
             int ig = int(255.99 * col.g());
             int ib = int(255.99 * col.b());
